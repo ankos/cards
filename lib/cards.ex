18f1c6a4f144d656c4b876f22312bbc05d3f94ee
André Koser <koser.andre@gmail.com>
2016-12-02 12:21:17 +0000

    add contains?

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,2 +1,13 @@
 defmodule Cards do
+  def create_deck do
+    ["Ace", "Three", "King", "Two"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+  
+  def contains?(deck, hand) do
+    
+  end
 end
